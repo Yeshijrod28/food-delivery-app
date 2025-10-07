491d6002a4cd32385a517813086f97465bc63c43
Yeshi Dorji <yeshijrod99@gmail.com>
2025-10-07 23:37:20 +0600

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-    const url = "https://food-delivery-app-qzc7.onrender.com/";
+    const url = "https://food-delivery-app-qzc7.onrender.com";
     return (
         <div>
             <ToastContainer/>
